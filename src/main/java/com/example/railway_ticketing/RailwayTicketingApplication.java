74583445ac3236abd837454b36b452c573ceb680
yx-Feng <1906574057@qq.com>
2024-09-11 13:10:54 +0800

    初始化

---
+++ src/main/java/com/example/railway_ticketing/RailwayTicketingApplication.java
@@ -0,0 +1,14 @@
+package com.example.railway_ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayTicketingApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RailwayTicketingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/railway_ticketing/RailwayTicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.railway_ticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayTicketingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
