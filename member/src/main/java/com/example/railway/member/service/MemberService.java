fdac643cf36d495b65a1c1106780de72178101b4
yx-Feng <1906574057@qq.com>
2024-09-14 22:18:58 +0800

    集成mybatis generator，初步设计会员注册接口

--- member/src/main/java/com/example/railway/member/service/MemberService.java
+++ member/src/main/java/com/example/railway/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.example.railway.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.example.railway.member.domain.Member;
+import com.example.railway.member.domain.MemberExample;
 import com.example.railway.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -11,6 +16,23 @@ public class MemberService {
     private MemberMapper memberMapper;
 
     public int count() {
-        return memberMapper.count();
+        return (int) memberMapper.countByExample(null);
+    }
+
+    public long register(String mobile) {
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list = memberMapper.selectByExample(memberExample);
+
+        if(CollUtil.isNotEmpty(list)) {
+            // return list.get(0).getId();
+            throw new RuntimeException("手机号已注册");
+        }
+
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
     }
 }
