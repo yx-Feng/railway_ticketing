0a8fa4eef99c2ca205a90da7f0d04916913ed166
yx-Feng <1906574057@qq.com>
2024-09-17 23:30:21 +0800

    提取CommonHeader和CommonSider公共组件

---
+++ web/src/components/CommonHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <a-layout-header class="header" id="components-layout-demo-top-side-2">
+      <div class="logo"/>
+      <a-menu v-model:selectedKeys="selectedKeys1" theme="dark" mode="horizontal" :style="{ lineHeight: '64px' }">
+        <a-menu-item key="1">nav 1</a-menu-item>
+        <a-menu-item key="2">nav 2</a-menu-item>
+        <a-menu-item key="3">nav 3</a-menu-item>
+      </a-menu>
+    </a-layout-header>
+  </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+const selectedKeys1 = ref(['2']);
+</script>
+
+<style scoped>
+#components-layout-demo-top-side-2 .logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+}
+
+.ant-row-rtl #components-layout-demo-top-side-2 .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
+}
+</style>
