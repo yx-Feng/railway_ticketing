cf7948462d370f86d4f4d33cd1a46e0860c476f3
yx-Feng <1906574057@qq.com>
2024-09-22 22:24:36 +0800

    解决表格中枚举字段（乘客类型）的显示

--- web/src/views/main/Passenger.vue
+++ web/src/views/main/Passenger.vue
@@ -15,6 +15,11 @@
           <a @click="onEdit(record)">编辑</a>
         </a-space>
       </template>
+      <template v-else-if="column.dataIndex === 'type'">
+        <span v-for="item in PASSENGER_TYPE_ARRAY" :key="item.key">
+          <span v-if="item.key === record.type">{{item.value}}</span>
+        </span>
+      </template>
     </template>
   </a-table>
   <a-modal v-model:open="visible" title="乘车人" @ok="handleOk" ok-text="确认" cancel-text="取消">
@@ -27,9 +32,7 @@
       </a-form-item>
       <a-form-item label="类型">
         <a-select v-model:value="passenger.type">
-          <a-select-option value="1">成人</a-select-option>
-          <a-select-option value="2">儿童</a-select-option>
-          <a-select-option value="3">学生</a-select-option>
+          <a-select-option v-for="item in PASSENGER_TYPE_ARRAY" :key="item.key" :value="item.key">{{item.value}}</a-select-option>
         </a-select>
       </a-form-item>
     </a-form>
@@ -81,6 +84,7 @@ const columns = [
     dataIndex: 'operation',
   },
 ]
+const PASSENGER_TYPE_ARRAY = [{key: "1", value: "成人"}, {key: "2", value: "儿童"}, {key: "3", value: "学生"}]
 
 // 新增乘车人
 const handleOk = (e) => {
