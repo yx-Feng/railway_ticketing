fc152a2cc8617874e28e47ecbff32f86f0a88c81
yx-Feng <1906574057@qq.com>
2024-10-03 15:06:11 +0800

    添加batch定时调度模块，新增batch数据库，配置持久层生成器，集成quartz和springboot自带的定时任务，共存

---
+++ batch/src/main/java/com/example/railway/batch/job/SpringBootTestJob.java
@@ -0,0 +1,17 @@
+package com.example.railway.batch.job;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+/**
+ * springboot自带定时任务，适合单体应用，不适合集群
+ */
+@Component
+@EnableScheduling
+public class SpringBootTestJob {
+
+    @Scheduled(cron = "0/5 * * * * ?")
+    private void test() {
+        System.out.println("SpringBootTestJob");
+    }
+}
