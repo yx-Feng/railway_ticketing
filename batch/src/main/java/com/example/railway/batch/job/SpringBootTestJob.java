4f051f0f3983dd099da81853b4e24799f0f71367
yx-Feng <1906574057@qq.com>
2024-10-03 15:23:11 +0800

    禁止定时任务的并发执行

--- batch/src/main/java/com/example/railway/batch/job/SpringBootTestJob.java
+++ batch/src/main/java/com/example/railway/batch/job/SpringBootTestJob.java
@@ -6,12 +6,12 @@ import org.springframework.stereotype.Component;
 /**
  * springboot自带定时任务，适合单体应用，不适合集群
  */
-@Component
-@EnableScheduling
-public class SpringBootTestJob {
-
-    @Scheduled(cron = "0/5 * * * * ?")
-    private void test() {
-        System.out.println("SpringBootTestJob");
-    }
-}
+//@Component
+//@EnableScheduling
+//public class SpringBootTestJob {
+//
+//    @Scheduled(cron = "0/5 * * * * ?")
+//    private void test() {
+//        System.out.println("SpringBootTestJob");
+//    }
+//}
--- batch/src/main/java/com/example/railway/batch/job/TestJob.java
+++ batch/src/main/java/com/example/railway/batch/job/TestJob.java
@@ -1,13 +1,21 @@
 package com.example.railway.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+@DisallowConcurrentExecution
 public class TestJob implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("TestJob");
+        System.out.println("TestJob开始");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("TestJob结束");
     }
 }
