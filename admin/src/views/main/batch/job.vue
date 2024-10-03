a5c714d6193042673dba87bece1f30eedf4b0dd0
yx-Feng <1906574057@qq.com>
2024-10-03 23:34:33 +0800

    增加任务手工补偿功能（能够立即执行一次任务）

--- admin/src/views/main/batch/job.vue
+++ admin/src/views/main/batch/job.vue
@@ -10,6 +10,9 @@
          <template #bodyCell="{ column, record }">
            <template v-if="column.dataIndex === 'operation'">
              <a-space>
+               <a-popconfirm title="手动执行会立即执行一次，确定执行?" ok-text="是" cancel-text="否" @confirm="handleRun(record)">
+                 <a-button type="primary" size="small">手动执行</a-button>
+               </a-popconfirm>
                <a-popconfirm title="确定重启？" ok-text="是" cancel-text="否" @confirm="handleResume(record)">
                  <a-button v-show="record.state === 'PAUSED' || record.state === 'ERROR'" type="primary" size="small">重启</a-button>
                </a-popconfirm>
@@ -189,6 +192,18 @@ const handleResume = (record) => {
   });
 };
 
+
+// 手动执行
+const handleRun = (record) => {
+  axios.post('/batch/admin/job/run', record).then((response) => {
+    const data = response.data;
+    if(data.success){
+      notification.success({description:"手动执行成功!"});
+    } else {
+      notification.error({description: data.message});
+    }
+  })
+}
 onMounted(() => {
   handleQuery();
 });
