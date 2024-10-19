d9c33a14af857cf2f920392b6064fb9f8cf696a9
yx-Feng <1906574057@qq.com>
2024-10-19 16:32:38 +0800

    SpringBoot整合redis缓存

--- gateway/src/main/java/com/example/railway/gateway/config/LoginMemberFilter.java
+++ gateway/src/main/java/com/example/railway/gateway/config/LoginMemberFilter.java
@@ -21,7 +21,8 @@ public class LoginMemberFilter implements Ordered, GlobalFilter {
         String path = exchange.getRequest().getURI().getPath();
 
         // 排除不需要拦截的请求
-        if (path.contains("/admin") || path.contains("/member/hello")
+        if (path.contains("/admin") || path.contains("/hello")
+                || path.contains("/redis")
                 || path.contains("/member/member/login")
                 || path.contains("/member/member/send-code")) {
             LOG.info("不需要登录验证：{}", path);
